7e57853c93a49bf5ae5b5d731089d03d67ffe2f2
saptiyani <110509181+saptiyani@users.noreply.github.com>
2022-08-11 20:55:55 -0700

    Add files via upload

---
+++ UPRAK_salsabila saptiyani_ X TKJ 1.py
@@ -0,0 +1,10 @@
+#tugas praktikum
+
+soal_no = input ("masukan no")
+nama_depan = input ("masukkan nama depan:")
+nama_belakang = input ("masukkan nama belakang")
+nama_anda = input ("masukkan nama_anda")
+soal_no = input ("masukkan no")
+celcius = int(input("masukkan suhu celcius: "))
+reamur = (4/5 * celcius) +32
+print (celcius,"celcius adalah", reamur,"reamur")
